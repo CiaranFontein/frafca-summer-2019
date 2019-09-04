29e000d4175a84f3de6c5c8c413fe1859df6c830
David Huynh <dh99.66.99@gmail.com>
2019-09-04 16:39:01 -0700

    :stuck_out_tongue_closed_eyes: : Change get_footer to wp_footer

--- themes/frafca/footer.php
+++ themes/frafca/footer.php
@@ -4,7 +4,6 @@
  *
  * @package FRAFCA_Theme
  */
-
 ?>
 
 			</div><!-- #content -->
@@ -12,7 +11,6 @@
 			<footer id="colophon" class="site-footer" role="contentinfo">
 
 			<nav id="footer-site-navigation" class="footer-navigation" role="navigation">
-<<<<<<< HEAD
 					<button class="menu-toggle" aria-controls="footer-menu" aria-expanded="false"><?php echo esc_html( 'Footer Menu - Connect' ); ?></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'footer_connect', 'menu_id' => 'footer-connect' ) ); ?>
 			</nav><!--site-navigation -->
@@ -26,22 +24,14 @@
 					<button class="menu-toggle" aria-controls="footer-menu" aria-expanded="false"><?php echo esc_html( 'Footer Menu - What We Do' ); ?></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'footer_what_we_do', 'menu_id' => 'footer-what-we-do' ) ); ?>
 			</nav><!--site-navigation -->
-=======
-					<button class="menu-toggle" aria-controls="footer-menu" aria-expanded="false"><?php echo esc_html( 'Footer Menu' ); ?></button>
-					<?php wp_nav_menu( array( 'theme_location' => 'footer', 'menu_id' => 'footer-menu' ) ); ?>
-			</nav>#site-navigation
->>>>>>> 5db6f3250cce16fae484eaad9ff55d7fdff3aa9c
-
-
-
-
+			
 				<div class="site-info">
 					<p> © 2019 FRAFCA copyright - All rights reserved </p>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
 
-		<?php get_footer(); ?>
+		<?php wp_footer(); ?>
 
 	</body>
 </html>
