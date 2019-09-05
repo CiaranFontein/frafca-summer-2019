bb27fadbfd940bd5cc883184dc0b068d6bca18ad
David Huynh <dh99.66.99@gmail.com>
2019-09-04 17:43:04 -0700

    :mans_shoe:: Updated footer.php

--- themes/frafca/footer.php
+++ themes/frafca/footer.php
@@ -12,24 +12,21 @@
 			<footer id="colophon" class="site-footer" role="contentinfo">
 
 			<nav id="footer-site-navigation" class="footer-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="footer-menu" aria-expanded="false"><?php echo esc_html( 'Footer Menu - Connect' ); ?></button>
+					<div class="footer-menu-header" aria-controls="footer-menu" aria-expanded="false"><?php echo esc_html( 'Connect' ); ?></div>
 					<?php wp_nav_menu( array( 'theme_location' => 'footer_connect', 'menu_id' => 'footer-connect' ) ); ?>
 			</nav><!--site-navigation -->
 
 			<nav id="footer-site-navigation" class="footer-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="footer-menu" aria-expanded="false"><?php echo esc_html( 'Footer Menu - Get Invloved' ); ?></button>
+					<div class="footer-menu-header" aria-controls="footer-menu" aria-expanded="false"><?php echo esc_html( 'Get Invloved' ); ?></div>
 					<?php wp_nav_menu( array( 'theme_location' => 'footer_get_involved', 'menu_id' => 'footer-get-involved' ) ); ?>
 			</nav><!--site-navigation -->
 
 			<nav id="footer-site-navigation" class="footer-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="footer-menu" aria-expanded="false"><?php echo esc_html( 'Footer Menu - What We Do' ); ?></button>
+					<div class="footer-menu-header" aria-controls="footer-menu" aria-expanded="false"><?php echo esc_html( 'What We Do' ); ?></div>
 					<?php wp_nav_menu( array( 'theme_location' => 'footer_what_we_do', 'menu_id' => 'footer-what-we-do' ) ); ?>
 			</nav><!--site-navigation -->
 
-
-
-
-				<div class="site-info">
+				<div class="copyright">
 					<p> © 2019 FRAFCA copyright - All rights reserved </p>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
