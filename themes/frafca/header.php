0534c9fcac3e1e9e134aec831da12f24b1266ac6
Ciaran Fontein <ciaranfontein@gmail.com>
2019-09-04 18:18:50 -0700

    🧭: Header navigation added to the functions.php

--- themes/frafca/header.php
+++ themes/frafca/header.php
@@ -14,6 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <?php wp_head() ?>
 </head>
 
 <body <?php body_class(); ?>>
@@ -27,10 +28,8 @@
             </div><!-- .site-branding -->
 
             <nav id="site-navigation" class="main-navigation" role="navigation">
-                <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
-                    <?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'menu_id' => 'header-menu' ) ); ?>
-                    <?php wp_nav_menu( array( 'theme_location' => 'side-header-menu', 'menu_id' => 'side-header-menu' ) ); ?>
-
+                <?php wp_nav_menu( array( 'theme_location' => 'Header-Menu', 'menu_id' => 'Header-Menu' ) ); ?>
+                <?php wp_nav_menu( array( 'theme_location' => 'Side-Header-Menu', 'menu_id' => 'Side-Header-Menu' ) ); ?>
             </nav><!-- #site-navigation -->
         </header><!-- #masthead -->
 
