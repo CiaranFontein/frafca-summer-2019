b32239442dd572233316fda1f7b4217e841f68e6
David Huynh <dh99.66.99@gmail.com>
2019-09-05 11:48:23 -0700

    :dancer:: create footer animation

--- themes/frafca/footer.php
+++ themes/frafca/footer.php
@@ -11,22 +11,34 @@
 
 		<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="footer-container">
-			<nav id="footer-site-navigation" class="footer-navigation" role="navigation">
-					<div class="footer-menu-header" aria-controls="footer-menu" aria-expanded="false"><?php echo esc_html( 'Connect' ); ?></div>
-					<?php wp_nav_menu( array( 'theme_location' => 'footer_connect', 'menu_id' => 'footer-connect' ) ); ?>
-					<i class="fas fa-angle-down"></i>
+			<nav id="footer-site-connect" class="footer-navigation" role="navigation">
+					<div class="footer-menu-trigger">
+						<div class="footer-menu-header" aria-controls="footer-menu" aria-expanded="false"><?php echo esc_html( 'Connect' ); ?></div>
+						<i class="fas fa-angle-down"></i>
+					</div>
+					<div class="footer-menu-container">
+						<?php wp_nav_menu( array( 'theme_location' => 'footer_connect', 'menu_id' => 'footer-connect' ) ); ?>
+					</div>
 			</nav><!--site-navigation -->
 
-			<nav id="footer-site-navigation" class="footer-navigation" role="navigation">
-					<div class="footer-menu-header" aria-controls="footer-menu" aria-expanded="false"><?php echo esc_html( 'Get Invloved' ); ?></div>
-					<?php wp_nav_menu( array( 'theme_location' => 'footer_get_involved', 'menu_id' => 'footer-get-involved' ) ); ?>
-					<i class="fas fa-angle-down"></i>
+			<nav id="footer-site-involved" class="footer-navigation" role="navigation">
+					<div class="footer-menu-trigger">
+						<div class="footer-menu-header" aria-controls="footer-menu" aria-expanded="false"><?php echo esc_html( 'Get Involved' ); ?></div>
+						<i class="fas fa-angle-down"></i>
+					</div>
+					<div class="footer-menu-container">
+						<?php wp_nav_menu( array( 'theme_location' => 'footer_get_involved', 'menu_id' => 'footer-get-involved' ) ); ?>
+					</div>
 			</nav><!--site-navigation -->
 
-			<nav id="footer-site-navigation" class="footer-navigation" role="navigation">
-					<div class="footer-menu-header" aria-controls="footer-menu" aria-expanded="false"><?php echo esc_html( 'What We Do' ); ?></div>
-					<?php wp_nav_menu( array( 'theme_location' => 'footer_what_we_do', 'menu_id' => 'footer-what-we-do' ) ); ?>
-					<i class="fas fa-angle-down"></i>
+			<nav id="footer-site-whatwedo" class="footer-navigation" role="navigation">
+					<div class="footer-menu-trigger">
+						<div class="footer-menu-header" aria-controls="footer-menu" aria-expanded="false"><?php echo esc_html( 'What We Do' ); ?></div>
+						<i class="fas fa-angle-down"></i>
+					</div>
+					<div class="footer-menu-container">
+						<?php wp_nav_menu( array( 'theme_location' => 'footer_what_we_do', 'menu_id' => 'footer-what-we-do' ) ); ?>
+					</div>
 			</nav><!--site-navigation -->
 
 			<div class="logo-social-container">
