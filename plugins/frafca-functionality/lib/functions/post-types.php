a6da320a9310b65ecf1175848a3f6da229beea7c
Yongmin Kim <yongmin86k@gmail.com>
2019-09-06 05:11:56 -0700

    :skull:: Update programs and services post-type to disable hierarchial category

--- plugins/frafca-functionality/lib/functions/post-types.php
+++ plugins/frafca-functionality/lib/functions/post-types.php
@@ -43,7 +43,7 @@ function frafca_post_type_programs_services() {
 		'description'           => __( 'A program and service post type for FRAFCA', 'text_domain' ),
 		'labels'                => $labels,
 		'supports'              => array( 'title', 'editor' ),
-		'taxonomies'            => array( 'post_tag' ),
+		'taxonomies'            => array( 'category' ),
 		'hierarchical'          => false,
 		'public'                => true,
 		'show_ui'               => true,
