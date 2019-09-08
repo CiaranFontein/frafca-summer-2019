32f3b4d3fbd0f89ecab444b0916ad5d29e9fad23
David Huynh <dh99.66.99@gmail.com>
2019-09-08 11:28:22 -0700

    🆕 :new:: created new page-get-involved.php

---
+++ themes/frafca/page-get-involved.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * The main template file.
+ * 
+ * @package FRAFCA_Theme
+ * Template Name: page-programs&services
+ */
+
+get_header(); ?>
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+
+        <!-- Hero Image : type 1  -->
+        <header id="prgrm_svc-banner"  class="frafca-hero-image">
+            <?php get_template_part( 'template-parts/hero_banner' ); ?>
+        </header><!-- #front-page-banner -->
+
+        <section id="prgrm_svc-categories">
+            <div class="grid-cards">
+                <?php  
+                    $prgrm_svc_categories = frafca_cfs('categories');
+                    foreach( $prgrm_svc_categories as $category ):
+                        $cat = get_term($category);
+                            $name = $cat->name;
+                            $description = $cat->description;
+                ?>
+                    <div class="rect-card purple">
+                        <h3><?php echo $name ?></h3>
+                        <p><?php echo $description ?></p>
+                        <a href="<?php echo get_term_link($cat) ?>" target="_blank">
+                            <input class='default-btn yellow' type="button" value="View Category">
+                        </a>
+                    </div>
+                <?php endforeach ?>
+            </div>
+        </section><!-- #prgrm_svc-categories -->
+
+    </main><!-- #main -->
+</div><!-- #primary -->
+
+
+<?php get_footer(); ?>
