3c5c77ef8730ed2468012d6089b749c409aa9ba2
Yongmin Kim <yongmin86k@gmail.com>
2019-09-11 16:21:30 -0700

    :skull:: Refactor hamburger-animations.js

--- themes/frafca/js/hamburger-animations.js
+++ themes/frafca/js/hamburger-animations.js
@@ -2,68 +2,38 @@
   let menuOpen = false;
   let $mobileHeaderMenu = $('.mobile-header-menu');
   let $hamburgerIcon = $('.hamburger-icon');
+  let lockScroll;
 
-  let scrollPosition = [
-    self.pageXOffset ||
-      document.documentElement.scrollLeft ||
-      document.body.scrollLeft,
-    self.pageYOffset ||
-      document.documentElement.scrollTop ||
-      document.body.scrollTop
-  ];
-  let html = $('html');
-  html.data('scroll-position', scrollPosition);
+  // Toggle the mobile-navigation
+  $(document).on('click', function(e){
+    let clicked_ele = e.target;
+    let checkIsInNav = $mobileHeaderMenu[0].contains(clicked_ele);
 
-  // Header hamburger drop down menu only on mobile //
-  $hamburgerIcon.on('click', function() {
-    toggleMobileNavMenu();
-  });
-
-  function toggleMobileNavMenu() {
-    $mobileHeaderMenu.animate({ width: 'toggle' }, 350);
-
-    if (menuOpen) {
-      menuOpen = false;
-      $mobileHeaderMenu.removeClass('mobile-header-menu--open');
-      $hamburgerIcon.removeClass('hamburger-icon--open');
-      unlockScrollPosition();
-    } else {
-      menuOpen = true;
-      $mobileHeaderMenu.addClass('mobile-header-menu--open');
-      $hamburgerIcon.addClass('hamburger-icon--open');
-      lockScrollPosition();
+    if ($hamburgerIcon[0].contains(clicked_ele)) {
+      toggleMobileNavMenu();
+      return
     }
-  }
-  function lockScrollPosition() {
-    // lock scroll position, but retain settings for later
-    scrollPosition = html.data('scroll-position');
-    html.data('previous-overflow', html.css('overflow'));
-    html.css('overflow', 'hidden');
-    window.scrollTo(scrollPosition[0], scrollPosition[1]);
-  }
-
-  function unlockScrollPosition() {
-    // un-lock scroll position
-    let html = $('html');
-    html.css('overflow', html.data('previous-overflow'));
-    window.scrollTo(scrollPosition[0], scrollPosition[1]);
-  }
-
-  /* Close mobile-nav-menu when user //
-  //     clicks outside of it        */
-
-  $hamburgerIcon.click(function() {
-    $mobileHeaderMenu.show();
-    return false;
-  });
-
-  $(document).click(function() {
-    if (menuOpen) {
+    
+    // Close the navigation when it's opened and clicked outside of the navigation
+    if (!checkIsInNav && menuOpen ){
       toggleMobileNavMenu();
     }
   });
 
-  $mobileHeaderMenu.click(function(e) {
-    e.stopPropagation();
+  function toggleMobileNavMenu() {
+    $mobileHeaderMenu.toggleClass('mobile-header-menu--open');
+    $hamburgerIcon.toggleClass('hamburger-icon--open');
+    menuOpen = !menuOpen;
+
+    lockScroll = $(window).scrollTop();
+  }
+  
+  // lock scroll position, but retain settings for later
+  $(window).on('scroll', function(){
+    if (menuOpen){
+      $(window).scrollTop(lockScroll);
+    }
+    
   });
+
 })(jQuery);
