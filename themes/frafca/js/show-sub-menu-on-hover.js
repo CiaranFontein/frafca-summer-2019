0fba0eea2ef722a09455464a0c2e9a3d0973c722
Yongmin Kim <yongmin86k@gmail.com>
2019-09-11 18:10:51 -0700

    :skull:: Create down-arrows when the menulist has sub-menu

--- themes/frafca/js/show-sub-menu-on-hover.js
+++ themes/frafca/js/show-sub-menu-on-hover.js
@@ -1,15 +1,30 @@
 (function($) {
-  $('li.menu-item').hover(
-    function() {
-      let $subMenu = $(this).find(' > .sub-menu');
-      $subMenu.slideDown(100); // display immediate child
-    },
-    function() {
-      // mouse leave
-      if (!$(this).hasClass('.current_page_item')) {
-        let $subMenu = $(this).find('.sub-menu');
-        $subMenu.slideUp(100); // hide if not current page
-      }
+
+  // Create arrow-down when the list has sub-menu
+  const $navSecondUL = $('#masthead').find('.menu-mobile-header-menu-container').children('ul').find('ul');
+  $navSecondUL.before(`<i class="fas fa-chevron-down"></i>`);
+
+  $(window).on('click hover', function(){
+
+    if ( $(window).width() < 1200 ){
+      // MOBILE functions
+
+    } else {
+      // DESKTOP functions
+      $('li.menu-item').hover(
+        function() {
+          let $subMenu = $(this).find(' > .sub-menu');
+          $subMenu.slideDown(100); // display immediate child
+        },
+        function() {
+          // mouse leave
+          if (!$(this).hasClass('.current_page_item')) {
+            let $subMenu = $(this).find('.sub-menu');
+            $subMenu.slideUp(100); // hide if not current page
+          }
+        }
+      );
+
     }
-  );
+  })
 })(jQuery);
