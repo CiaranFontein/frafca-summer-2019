75c09188d3bbe996b6698f69abb1ff534358c602
Yongmin Kim <yongmin86k@gmail.com>
2019-09-11 20:04:16 -0700

    :skull:: Open and close animation with click event on Mobile submenu

--- themes/frafca/js/show-sub-menu-on-hover.js
+++ themes/frafca/js/show-sub-menu-on-hover.js
@@ -1,6 +1,7 @@
 (function($) {
 
   // Create arrow-down when the list has sub-menu
+  const animateTime = 100;
   const $navSecondUL = $('#masthead').find('.menu-mobile-header-menu-container').children('ul').find('ul');
   $navSecondUL.before(`<i class="fas fa-chevron-down"></i>`);
 
@@ -8,23 +9,27 @@
     let targetEle = e.target;
     if ( $(window).width() < 1200 ){
       // MOBILE functions
-      if ($(targetEle).hasClass('fa-chevron-down') ){
-        console.log(targetEle);
+      
+      // Toggle the submenu and rotate the arrow icon
+      if ( e.type === 'click' && $(targetEle).hasClass('fa-chevron-down') ){
+        let $mobileSubMenu = $(targetEle).siblings('ul');
+        $(targetEle).toggleClass('open');
+        $mobileSubMenu.slideToggle(animateTime);
+
       }
-            
 
     } else {
       // DESKTOP functions
       $('li.menu-item').hover(
         function() {
           let $subMenu = $(this).find(' > .sub-menu');
-          $subMenu.slideDown(100); // display immediate child
+          $subMenu.slideDown(animateTime); // display immediate child
         },
         function() {
           // mouse leave
           if (!$(this).hasClass('.current_page_item')) {
             let $subMenu = $(this).find('.sub-menu');
-            $subMenu.slideUp(100); // hide if not current page
+            $subMenu.slideUp(animateTime); // hide if not current page
           }
         }
       );
