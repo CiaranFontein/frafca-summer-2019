5a4eced5e6658b80dc15cbaa0788277ce61fd8ca
Yongmin Kim <yongmin86k@gmail.com>
2019-09-12 21:38:55 -0700

    :skull:: Bind the flickity functions to Front-page

--- themes/frafca/functions.php
+++ themes/frafca/functions.php
@@ -110,14 +110,18 @@ function frafca_theme_scripts()
 	// Enqueue CF7 script after submit button is clicked
 	wp_enqueue_script('frafca-theme-cf7-event-submit', get_template_directory_uri() . '/build/js/cf7-event-submit.min.js', array('jquery'), '', true);
 	// Scripts for Carousels - flickity
-	wp_enqueue_style('frafca-theme-style', get_stylesheet_uri());
-	wp_enqueue_style('flickity-style', 'https://unpkg.com/flickity@2/dist/flickity.min.css');
-	wp_register_script('flickity', 'https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js', null, null, true);
+	if ( is_front_page() ) {
 
-	wp_enqueue_script('frafca-carousel', get_template_directory_uri() . '/build/js/carousel.min.js', array('jquery', 'flickity'), '', true);
-
+		wp_enqueue_style('flickity-style', 'https://unpkg.com/flickity@2/dist/flickity.min.css');
+		wp_register_script('flickity', 'https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js', null, null, true);
+		
+		wp_enqueue_script('frafca-carousel', get_template_directory_uri() . '/build/js/carousel.min.js', array('jquery', 'flickity'), '', true);
+	}
+	
 	wp_enqueue_script('frafca-theme-navigation', get_template_directory_uri() . '/build/js/navigation.min.js', array(), '20151215', true);
 	wp_enqueue_script('frafca-theme-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20151215', true);
+	
+	wp_enqueue_style('frafca-theme-style', get_stylesheet_uri());
 
 	if (is_singular() && comments_open() && get_option('thread_comments')) {
 		wp_enqueue_script('comment-reply');
