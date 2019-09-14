57596d0d475288f51af8d4643bd89879a686748d
David Huynh <dh99.66.99@gmail.com>
2019-09-14 00:26:30 -0700

    :tired_face:: styling the calendar even page by copy a month folder from even calendar plug in

--- themes/frafca/tribe-events/month/nav.php
+++ themes/frafca/tribe-events/month/nav.php
@@ -16,13 +16,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <?php do_action( 'tribe_events_before_nav' ) ?>
 
-<nav class="tribe-events-nav-pagination" aria-label="<?php esc_html_e( 'Calendar Month Navigation', 'the-events-calendar' ) ?>">
-	<ul class="tribe-events-sub-nav">
-		<li class="tribe-events-nav-previous">
+<nav class="tribe-events-nav-pagination frafca-events-nav-pagination" aria-label="<?php esc_html_e( 'Calendar Month Navigation', 'the-events-calendar' ) ?>">
+	<ul class="tribe-events-sub-nav frafca-events-sub-nav">
+		<li class="tribe-events-nav-previous frafca-events-nav-previous">
 			<?php tribe_events_the_previous_month_link(); ?>
 		</li>
 		<!-- .tribe-events-nav-previous -->
-		<li class="tribe-events-nav-next">
+		<li class="tribe-events-nav-next frafca-events-nav-next">
 			<?php tribe_events_the_next_month_link(); ?>
 		</li>
 		<!-- .tribe-events-nav-next -->
