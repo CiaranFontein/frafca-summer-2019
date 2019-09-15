e51a9df5222c1aacd98726da20ca4ac54b0a265b
AdhamElNewihy <adham.el.newihy@gmail.com>
2019-09-14 18:41:05 -0700

    :rainbow:: Add buttons in center of carousel on mobile mode

--- themes/frafca/js/carousel.js
+++ themes/frafca/js/carousel.js
@@ -1,7 +1,7 @@
 (function($) {
   $(document).ready(function() {
     if (window.innerWidth <= 1000) {
-      const content = document.querySelector('.fp-about-us-carousel');
+      const content = document.querySelector('.us-carousel');
       const flkty = new Flickity(content, {
         // carousel options
         cellAlign: 'center',
