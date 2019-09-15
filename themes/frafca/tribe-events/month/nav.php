6582df69bae588a8e332cf3ede57cde6795bede9
David Huynh <dh99.66.99@gmail.com>
2019-09-15 14:41:20 -0700

    :angry:: style mobile events calendar

--- themes/frafca/tribe-events/month/nav.php
+++ themes/frafca/tribe-events/month/nav.php
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <nav class="tribe-events-nav-pagination frafca-events-nav-pagination" aria-label="<?php esc_html_e( 'Calendar Month Navigation', 'the-events-calendar' ) ?>">
 	<ul class="tribe-events-sub-nav frafca-events-sub-nav">
 		<li class="tribe-events-nav-previous frafca-events-nav-previous">
-			<?php tribe_events_the_previous_month_link(); ?>
+		<i class="fas fa-angle-left"></i><?php tribe_events_the_previous_month_link(); ?>
 		</li>
 		<!-- Month Title -->
 		<?php do_action( 'tribe_events_before_the_title' ); ?>
@@ -27,7 +27,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<?php do_action( 'tribe_events_after_the_title' ); ?>
 		<!-- .tribe-events-nav-previous -->
 		<li class="tribe-events-nav-next frafca-events-nav-next">
-			<?php tribe_events_the_next_month_link(); ?>
+		<i class="fas fa-angle-right"></i><?php tribe_events_the_next_month_link(); ?>
 		</li>
 		<!-- .tribe-events-nav-next -->
 	</ul><!-- .tribe-events-sub-nav -->
