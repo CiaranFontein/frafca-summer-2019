1707e5e53dcad2ebb8a7866357b265d55ba7cf60
AdhamElNewihy <adham.el.newihy@gmail.com>
2019-09-15 19:51:54 -0700

    :mag_right:: Add search count

--- themes/frafca/search.php
+++ themes/frafca/search.php
@@ -15,10 +15,15 @@ get_header(); ?>
 
 			<header class="page-header">
 				<h1 class="page-title"><?php printf(esc_html('Search Results for: %s'), '<span>' . get_search_query() . '</span>'); ?></h1>
+				<div class="search-count">
+					<?php $allsearch = new WP_Query("s=$s&showposts=0");
+						echo $allsearch->found_posts . ' results found.'; ?>
+				</div>
 			</header><!-- .page-header -->
 
 			<?php /* Start the Loop */ ?>
 			<?php while (have_posts()) : the_post(); ?>
+
 
 				<?php get_template_part('template-parts/content', 'search'); ?>
 
