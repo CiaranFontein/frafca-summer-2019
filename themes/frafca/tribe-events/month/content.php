6d1b7108419adacb19a8aa82c3c27ed9d1888321
Yongmin Kim <yongmin86k@gmail.com>
2019-09-17 15:26:37 -0700

    :skull:: Save before deploy

--- themes/frafca/tribe-events/month/content.php
+++ themes/frafca/tribe-events/month/content.php
@@ -16,9 +16,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 } ?>
 
 <div id="tribe-events-content" class="tribe-events-month">
-
 	<!-- Notices -->
 	<?php tribe_the_notices() ?>
+
 
 	<!-- Month Header -->
 	<?php do_action( 'tribe_events_before_header' ) ?>
@@ -50,31 +50,57 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<?php tribe_get_template_part( 'month/tooltip' ); ?>
 </div><!-- #tribe-events-content -->
 
-
 <section class="tribe-events-listing">
+	<?php
+	global $post;
+	// $month_start = date('Y-m') . '-01';
+	// $month_end = date("Y-m-t");
+	$events = tribe_get_events([ 
+		'posts_per_page' => 5,
+		// 'start_date'   => $month_start,
+		// 'end_date'     => $month_end,
+		]);
+	?>
 
+	<div class="header-current-month">
+
+	</div>
+		
 		<?php
-				global $post;
-				$month_start = date('Y-m') . '-01';
-				$month_end = date("Y-m-t");
-				$events = tribe_get_events([ 
-					'posts_per_page' => 5,
-					'start_date'   => $month_start,
-   					'end_date'     => $month_end,
-				]);
+		// var_dump($month_start, $month_end);
 
-				// var_dump($month_start, $month_end);
-
-				foreach ( $events as $post ) {
-				setup_postdata( $post );
-				echo '<div class="frafca-events-listing-mobile">';
-				echo '<h4><a href="'. get_the_permalink( $post ). '">' . ($post->post_title) . '</a></h4>';
-				// echo '<h4>' . get_the_permalink( $post ) . '</h4>';
-				echo '<p>' . tribe_get_start_date( $post ) . ' - ' . tribe_get_end_time( $post ) . '</p>';
-				echo '<p>' . tribe_get_address( $post ) . ' ' . tribe_get_city( $post ) . '</p>';
-				// echo "<a href='".get_the_permalink( $post )."'>Link</a>";
-				echo '</div>';
-		}
+		foreach ( $events as $post ) :
+			setup_postdata( $post );
+			$title = $post -> post_title;
+			$start_date = tribe_get_start_date( $post );
+			$end_date = tribe_get_end_time( $post );
+			$event_address = tribe_get_address( $post );
+			$event_city = tribe_get_city( $post );
+			// var_dump($post);
 		?>
-
+			<div class="frafca-events-listing-mobile">
+				<h4>
+					<a href="<?php echo get_the_permalink( $post );?>"> 
+						<?php echo $title ?>
+					</a>
+				</h4>
+				
+				<p>
+					<!-- Check if it's all day event -->
+					<?php 
+					if ( !empty( $end_date ) ){
+						echo "$start_date - $end_date";
+					} else {
+						echo $start_date;
+					}
+					?>
+				</p>
+				<!-- Check if event_city exist -->
+				<?php if ( !empty( $event_city )) : ?>
+					<p><?php echo "$event_address, $event_city"; ?></p>
+				<?php elseif ( !empty( $event_address ) ) : ?>
+					<p><?php echo $event_address; ?></p>
+				<?php endif; ?>
+			</div>
+		<?php endforeach; ?>
 </section> <!-- .tribe-events-listinge.end -->
