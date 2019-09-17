856aac94034ef960d9dd06ad31d30618e8455a3d
David Huynh <dh99.66.99@gmail.com>
2019-09-17 15:12:42 -0700

    :rooster:: style the main calendar events page

--- themes/frafca/js/cf7-event-submit.js
+++ themes/frafca/js/cf7-event-submit.js
@@ -1,9 +1,9 @@
 (function ($) {
     // Form will be hidden after the mail is sent
-    document.addEventListener( 'wpcf7mailsent', function() {
+    document.addEventListener('wpcf7mailsent', function () {
         const $form = $('#content-get-in-touch');
         const animationTime = 400;
         $form.slideUp(animationTime);
-    }, false );
+    }, false);
 
 })(jQuery);
