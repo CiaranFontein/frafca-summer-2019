1309db45c12a4a494e1cc8c7a990eb5046503ded
Ciaran Fontein <ciaranfontein@gmail.com>
2019-09-17 15:55:23 -0700

    🌚: Header changes color and scroll is not passive

--- themes/frafca/js/header-change-on-scroll.js
+++ themes/frafca/js/header-change-on-scroll.js
@@ -1,21 +1,17 @@
 (function($) {
   let $header = $('.site-header');
-  $(document).on(
-    'scroll',
-    function() {
-      if ($(window).scrollTop() > 0) {
-        if ($(window).scrollTop() > $('.frafca-hero-image').innerHeight()) {
-          $header.addClass('darkmode');
-          $header.removeClass('mediummode');
-        } else {
-          $header.addClass('mediummode');
-          $header.removeClass('darkmode');
-        }
-      } else {
-        $header.removeClass('darkmode');
+  $(document).on('scroll', function() {
+    if ($(window).scrollTop() > 0) {
+      if ($(window).scrollTop() > $('.frafca-hero-image').innerHeight()) {
+        $header.addClass('darkmode');
         $header.removeClass('mediummode');
+      } else {
+        $header.addClass('mediummode');
+        $header.removeClass('darkmode');
       }
-    },
-    { passive: true }
-  );
+    } else {
+      $header.removeClass('darkmode');
+      $header.removeClass('mediummode');
+    }
+  });
 })(jQuery);
