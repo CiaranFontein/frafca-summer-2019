f12da9e6da6323dec5a29bbe1e598256e686f68a
Ciaran Fontein <ciaranfontein@gmail.com>
2019-09-17 15:53:26 -0700

    🛂: Scroll event listener for header changing is passive

--- themes/frafca/js/header-change-on-scroll.js
+++ themes/frafca/js/header-change-on-scroll.js
@@ -1,17 +1,21 @@
 (function($) {
   let $header = $('.site-header');
-  $(document).on('scroll', function() {
-    if ($(window).scrollTop() > 0) {
-      if ($(window).scrollTop() > $('.frafca-hero-image').innerHeight()) {
-        $header.addClass('darkmode');
-        $header.removeClass('mediummode');
+  $(document).on(
+    'scroll',
+    function() {
+      if ($(window).scrollTop() > 0) {
+        if ($(window).scrollTop() > $('.frafca-hero-image').innerHeight()) {
+          $header.addClass('darkmode');
+          $header.removeClass('mediummode');
+        } else {
+          $header.addClass('mediummode');
+          $header.removeClass('darkmode');
+        }
       } else {
-        $header.addClass('mediummode');
         $header.removeClass('darkmode');
+        $header.removeClass('mediummode');
       }
-    } else {
-      $header.removeClass('darkmode');
-      $header.removeClass('mediummode');
-    }
-  });
+    },
+    { passive: true }
+  );
 })(jQuery);
