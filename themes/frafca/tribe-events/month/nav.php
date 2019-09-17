c33609743b71ed90d961dff0d47c57fc06c7ffdf
Yongmin Kim <yongmin86k@gmail.com>
2019-09-17 13:39:50 -0700

    :skull:: Create separated scss file for the event calendar

--- themes/frafca/tribe-events/month/nav.php
+++ themes/frafca/tribe-events/month/nav.php
@@ -23,7 +23,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 		</li>
 		<!-- Month Title -->
 		<?php do_action( 'tribe_events_before_the_title' ); ?>
-		<h1 class="tribe-events-page-title frafca-events-page-title"><?php echo tribe_get_events_title() ?></h1>
+		<h1 class="tribe-events-page-title frafca-events-page-title">
+			<?php echo tribe_get_events_title() ?>
+		</h1>
 		<?php do_action( 'tribe_events_after_the_title' ); ?>
 		<!-- .tribe-events-nav-previous -->
 		<li class="tribe-events-nav-next frafca-events-nav-next">
@@ -32,5 +34,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<!-- .tribe-events-nav-next -->
 	</ul><!-- .tribe-events-sub-nav -->
 </nav>
-<?php
-do_action( 'tribe_events_after_nav' );
+<?php do_action( 'tribe_events_after_nav' );
