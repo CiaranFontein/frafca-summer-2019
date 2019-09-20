6cde1e7c74446221a97145fa0baa179401cb5f8d
AdhamElNewihy <adham.el.newihy@gmail.com>
2019-09-20 00:01:28 -0700

    :pencil2:: Code clean up

--- themes/frafca/js/carousel.js
+++ themes/frafca/js/carousel.js
@@ -4,15 +4,11 @@
   $(document).ready(function() {
     const $fpAboutUsContent = $('.fp-about-us-carousel');
 
-    // Bind the function only when it's mobile
     stopFlickty($fpAboutUsContent);
-    
-    $(window).on('resize', function(){
+
+    $(window).on('resize', function() {
       stopFlickty($fpAboutUsContent);
     });
-    
-
-
 
     const fpPartnersContent = document.querySelector('.fp-partners-carousel');
     const fpPartners = new Flickity(fpPartnersContent, {
@@ -20,20 +16,18 @@
       cellAlign: 'left',
       wrapAround: true,
       selectedAttraction: 0.01,
-      // friction: 0.15,
       autoPlay: 2000
     });
-
   }); // End of Document.Ready
 
-
-
-  function stopFlickty($ele){
+  // Refactored Functions
+  function stopFlickty($ele) {
     let curBrowserWidth = $(window).width();
-      
-    if ( curBrowserWidth < breakPoint ){
-        /* Mobile flickity */
-        $ele.flickity({
+
+    if (curBrowserWidth < breakPoint) {
+      /* Mobile flickity */
+      $ele
+        .flickity({
           cellAlign: 'center',
           wrapAround: true,
           selectedAttraction: 0.01,
@@ -41,39 +35,14 @@
           autoPlay: 3000,
           draggable: '>1',
           resize: true,
-          setGallerySize: true,
+          setGallerySize: true
         })
         .flickity('playPlayer')
         .flickity('reposition');
-      
-      } else {
-        /* Desktop flickity  */
-        
-        // Stop flickity functions
-        $ele
-        .flickity('destroy');
-        // .flickity({
-        //   cellAlign: 'center',
-        //   wrapAround: true,
-        //   selectedAttraction: 0.01,
-        //   friction: 0.15,
-        //   draggable: false,
-        //   setGallerySize: false,
-        // })
-        // .flickity('stopPlayer')
-        // .flickity('reposition');
-
-        // reset css
-        // $ele.find('.flickity-viewport').css({
-        //   'height': 440
-        // });
-        // console.log(1);
-        // $ele.find('.flickity-slider').css('transform', 'translateX(35.17%)');
-        // $ele.find('.flickity-slider').children().eq(0).css('left', 0);
-        // $ele.find('.flickity-slider').children().eq(1).css('left', '29.67%');
-        // $ele.find('.flickity-slider').children().eq(2).css('left', '-29.67%');
-        
-      }
-  }
-
+    } else {
+      /* Desktop flickity  */
+      // Stop flickity functions
+      $ele.flickity('destroy');
+    }
+  } // End of Functions StopFlickity
 })(jQuery);
